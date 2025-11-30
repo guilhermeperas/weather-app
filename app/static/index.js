9284c769b8a529950eecb9fbd73a06bfbe6cf5b3
guilhermeperas <pguilherme926@gmail.com>
2025-11-30 00:05:04 +0000

    js

--- app/static/index.js
+++ app/static/index.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('weather-form');
-const cityInput = document.getElementById('city');
+
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
 });
